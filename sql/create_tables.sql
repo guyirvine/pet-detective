dcbbe346d09da28e0a4e2af34a5b629a4cac3728
Guy Irvine <girvine@lic.co.nz>
2015-03-30 21:17:33 +1300

    Cleared create_table script

--- sql/create_tables.sql
+++
@@ -1,8 +0,0 @@
-CREATE SEQUENCE alert_seq;
-
-CREATE TABLE alert_tbl(
-  id BIGINT NOT NULL DEFAULT NEXTVAL( 'alert_seq' ),
-  observation_id BIGINT NOT NULL,
-  sensor_key VARCHAR NOT NULL,
-  clearedat TIMESTAMP
-);
---
+++ sql/create_tables.sql.20150330.1
@@ -0,0 +1,8 @@
+CREATE SEQUENCE alert_seq;
+
+CREATE TABLE alert_tbl(
+  id BIGINT NOT NULL DEFAULT NEXTVAL( 'alert_seq' ),
+  observation_id BIGINT NOT NULL,
+  sensor_key VARCHAR NOT NULL,
+  clearedat TIMESTAMP
+);
